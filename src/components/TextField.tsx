5bf16eaac8228e072aa5ff14a62ca6b9e81aa4d0
UshanKaushalya <Ushanwk22@gmail.com>
2024-01-13 11:38:20 +0530

    sample login page

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,5 +1,18 @@
+import {Text, TextInput, View} from "react-native";
+import React from "react";
+
+type TextFieldProps = {
+
+}
+
 export function TextField() {
     return (
-        <></>
+        <View>
+            <Text style={{marginLeft: 4, marginBottom: 8, color: 'black'}}>Username</Text>
+
+            <TextInput
+                style={{borderColor: 'gray', borderWidth: 1, borderRadius: 10}}
+            />
+        </View>
     );
 }
--- src/pages/SigninPage.tsx
+++ src/pages/SigninPage.tsx
@@ -1,10 +1,15 @@
 import {Text, View} from "react-native";
+import {TextField} from "../components/TextField.tsx";
 
 export function SigninPage() {
     return (
-        <View style={{marginVertical: 25, marginHorizontal: 15}}>
+        <View style={{marginVertical: 25, marginHorizontal: 25}}>
 
             <Text style={{fontSize: 35, color: 'black', textAlign: 'center', fontWeight: 'bold'}}>Sign In</Text>
+
+            <View style={{marginTop: 45}}>
+                <TextField/>
+            </View>
 
         </View>
     );
