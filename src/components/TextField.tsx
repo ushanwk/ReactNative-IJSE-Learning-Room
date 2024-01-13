6be43ce83310f55f71455ab519f131aefc7be950
UshanKaushalya <Ushanwk22@gmail.com>
2024-01-13 11:50:55 +0530

    sample login page

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -2,16 +2,20 @@ import {Text, TextInput, View} from "react-native";
 import React from "react";
 
 type TextFieldProps = {
-
+    label: string;
+    value: string;
+    setValue: (value: string)=>void
 }
 
-export function TextField() {
+export function TextField(props:TextFieldProps) {
     return (
-        <View>
-            <Text style={{marginLeft: 4, marginBottom: 8, color: 'black'}}>Username</Text>
+        <View style={{marginBottom: 30}}>
+            <Text style={{marginLeft: 4, marginBottom: 8, color: 'black'}}>{props.label}</Text>
 
             <TextInput
-                style={{borderColor: 'gray', borderWidth: 1, borderRadius: 10}}
+                style={{borderColor: 'gray', borderWidth: 1, borderRadius: 10, padding: 10}}
+                value={props.value}
+                onChange={(event)=>{props.setValue(event.nativeEvent.text)}}
             />
         </View>
     );
--- src/pages/SigninPage.tsx
+++ src/pages/SigninPage.tsx
@@ -1,14 +1,29 @@
 import {Text, View} from "react-native";
 import {TextField} from "../components/TextField.tsx";
+import {useState} from "react";
 
 export function SigninPage() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <View style={{marginVertical: 25, marginHorizontal: 25}}>
 
             <Text style={{fontSize: 35, color: 'black', textAlign: 'center', fontWeight: 'bold'}}>Sign In</Text>
 
             <View style={{marginTop: 45}}>
-                <TextField/>
+                <TextField
+                    label="Username"
+                    value={username}
+                    setValue={(value)=>{setUsername(value)}}
+                />
+
+                <TextField
+                    label="Password"
+                    value={password}
+                    setValue={(value)=>{setPassword(value)}}
+                />
             </View>
 
         </View>
